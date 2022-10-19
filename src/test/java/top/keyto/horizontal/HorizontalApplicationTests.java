c18450ec1377124b1d754208dffe907c2b38f873
Keyto <keyto1995@outlook.com>
2022-10-19 21:09:46 +0800

    init project;

---
+++ src/test/java/top/keyto/horizontal/HorizontalApplicationTests.java
@@ -0,0 +1,13 @@
+package top.keyto.horizontal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorizontalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
