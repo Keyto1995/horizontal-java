45f7ca1f1fd263801a665d97e8ef3869dfd040d2
Keyto <keyto1995@outlook.com>
2022-10-19 21:13:26 +0800

    feat: 添加editorconfig配置;

--- src/main/java/top/keyto/horizontal/HorizontalApplication.java
+++ src/main/java/top/keyto/horizontal/HorizontalApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HorizontalApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(HorizontalApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(HorizontalApplication.class, args);
+  }
 
 }
